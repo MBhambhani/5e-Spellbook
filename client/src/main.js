a30ec854cf7ebff9259abd7849209c9ff997613e
Milind Bhambhani <mcbhambh@uwaterloo.ca>
2019-07-02 23:58:32 -0400

    added vueify, material design icons, axios

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
+Vue.use(Vuetify, {
+  iconfont: 'md',
+});
 Vue.config.productionTip = false;
 
 new Vue({
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-expansion-panel>
+    <v-expansion-panel-content>
+      <template v-slot:header>
+        <div>Cantrips</div>
+      </template>
+      <v-card>
+        <v-card-text>{{ msg }}</v-card-text>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
 </template>
 
 <script>
+import axios from 'axios';
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://127.0.0.1:5000/ping';
+      axios.get(path)
+        .then((response) => {
+          this.msg = response.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
   },
 };
 </script>
